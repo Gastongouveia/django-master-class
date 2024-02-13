1155aa437c3675b3cae69d386e1cc90fa9f0ce58
Gaston Gouveia <gastongouveia@gmail.com>
2024-02-12 23:19:14 -0300

    create project apps

--- django_masterclass/settings.py
+++ django_masterclass/settings.py
@@ -31,12 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # third-party
+    'rest_framework',
+    
+    # local
+    'blog'
+    'user'
 ]
 
 MIDDLEWARE = [
