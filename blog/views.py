3e0cb80fe052dfc9b3cf8883ffe53c6aea331845
Gaston Gouveia <gastongouveia@gmail.com>
2024-07-15 22:56:23 -0300

    update view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from rest_framework.response import Response
 from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import BlogPost
 from .serializers import BlogPostSerializer
@@ -7,4 +7,8 @@ from .serializers import BlogPostSerializer
 class BlogPostList(ListAPIView):
     queryset = BlogPost.objects.filter(published_at__isnull=False)
     serializer_class = BlogPostSerializer
+    ordering = ['-created_at']
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'body']
+    ordering_fields = ['created_at','published_at', 'title']
 
